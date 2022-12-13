c1fec82d46dafa96c1b5057e5d06d03286cbf96e
Marco Antonio <caldeiran391@gmail.com>
2022-12-12 22:04:24 -0300

    adicionando arquivos

---
+++ Exercicio2_Objetos/Exercicio2_Objetos/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace Exercicio2_Objetos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.text_nome = new System.Windows.Forms.TextBox();
+            this.text_preco = new System.Windows.Forms.TextBox();
+            this.text_quant = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.list_produtos = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Preço";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 209);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Quantidade de estoque:";
+            // 
+            // text_nome
+            // 
+            this.text_nome.Location = new System.Drawing.Point(77, 64);
+            this.text_nome.Name = "text_nome";
+            this.text_nome.Size = new System.Drawing.Size(236, 20);
+            this.text_nome.TabIndex = 3;
+            // 
+            // text_preco
+            // 
+            this.text_preco.Location = new System.Drawing.Point(77, 132);
+            this.text_preco.Name = "text_preco";
+            this.text_preco.Size = new System.Drawing.Size(236, 20);
+            this.text_preco.TabIndex = 4;
+            // 
+            // text_quant
+            // 
+            this.text_quant.Location = new System.Drawing.Point(163, 206);
+            this.text_quant.Name = "text_quant";
+            this.text_quant.Size = new System.Drawing.Size(150, 20);
+            this.text_quant.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(373, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 39);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Cadastrar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(373, 57);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 39);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Consultar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(373, 102);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(118, 39);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Efetuar Venda";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(373, 147);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(118, 39);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Listar Todos os refrigerantes";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(373, 192);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(118, 39);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Excluir Refrigerante";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(373, 237);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(118, 39);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Sair";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // list_produtos
+            // 
+            this.list_produtos.FormattingEnabled = true;
+            this.list_produtos.Location = new System.Drawing.Point(39, 250);
+            this.list_produtos.Name = "list_produtos";
+            this.list_produtos.Size = new System.Drawing.Size(274, 95);
+            this.list_produtos.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 375);
+            this.Controls.Add(this.list_produtos);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.text_quant);
+            this.Controls.Add(this.text_preco);
+            this.Controls.Add(this.text_nome);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox text_nome;
+        private System.Windows.Forms.TextBox text_preco;
+        private System.Windows.Forms.TextBox text_quant;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.ListBox list_produtos;
+    }
+}
+
